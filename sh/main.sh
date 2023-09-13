1009d39cf939548975917c7dda597b77c390a702
Aivaras Baranauskas <5412767+aivarasbaranauskas@users.noreply.github.com>
2023-09-13 15:48:12 +0300

    nicer awk solution

--- sh/main.sh
+++ sh/main.sh
@@ -1,37 +1,27 @@
 #!/usr/bin/awk -f
 {
     s = 0;
-    noZero = 1;
-    for(i=3; i<=NF; i++) {
-        if ($i == 0) {
-            noZero = 0;
-        }
+    for(i = 3; i <= NF; i++) {
+        if ($i == 0) { next }
         s += $i * $2;
     }
 
-    if (noZero == 1) {
-        found = 0
-        k = 0
-        for (i in names) {
-            if (names[i] == $1) {
-                all[i] += s;
-                noZeros[i] += noZero;
-                found = 1;
-            }
-            k++
-        }
-
-        if (found == 0) {
-            names[k] = $1
-            all[k] = s;
-            noZeros[k] = noZero;
+    for (i in names) {
+        if (names[i] == $1) {
+            all[i] += s;
+            noZeros[i] ++;
+            next
         }
     }
+
+    l = length(names)
+    names[l] = $1
+    all[l] = s;
+    noZeros[l] = 1;
 }
 END{
-    k = 0
-    for (i in names) { k++ }
-    for (i=0 ; i<k ; i++) {
+    l = length(names)
+    for (i=0 ; i<l ; i++) {
         printf "%s %d %.2f\n", names[i], noZeros[i], all[i]/100000;
     }
 }
